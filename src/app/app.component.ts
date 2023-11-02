00ec20101ed44d213f670cb91eea0718abdd895e
cretara <pierdanielecretara@lviiier.com>
2023-11-02 21:59:42 +0100

    first commit con Angular v17 new features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { User } from './model';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Angular17RC';
+  public flagTest: boolean = true;
+
+  public arrayTest: User[] = [
+    { id: 1, name: 'user' },
+    { id: 2, name: 'mario' },
+    { id: 3, name: 'rossi' },
+  ];
 }
